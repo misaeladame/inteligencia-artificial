93b6000065322223d5f44d797f404d6170f6700c
Misael Adame <misael_adame@protonmail.com>
2021-09-28 20:32:52 -0500

    Triangulo

---
+++ Unidad 1/Ejercicios Python/03-ejercicios de bucles/06.py
@@ -0,0 +1,10 @@
+"""Escribir un programa que pida al usuario un número entero
+y muestre por pantalla un triángulo rectángulo como el de
+más abajo, de altura el número introducido."""
+
+numero = int(input("Introduce un numero entero (la altura del triangulo): "))
+
+for i in range(numero):
+    for j in range(i + 1):
+        print("*", end="")
+    print()
