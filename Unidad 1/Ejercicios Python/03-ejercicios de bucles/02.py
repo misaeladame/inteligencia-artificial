db6ecc7063ef596f1da63cedc60932e3e60664a1
Misael Adame <misael_adame@protonmail.com>
2021-09-28 19:58:45 -0500

    Edades

---
+++ Unidad 1/Ejercicios Python/03-ejercicios de bucles/02.py
@@ -0,0 +1,8 @@
+"""Escribir un programa que pregunte al usuario su edad y
+muestre por pantalla todos los años que ha cumplido
+(desde 1 hasta su edad)"""
+
+edad = int(input("¿Cuál es tu edad? "))
+
+for i in range(edad):
+    print(i+1)
